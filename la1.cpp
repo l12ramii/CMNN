273403216eaee4e3f6394854200c36c559594972
l12ramii <l12ramii@uco.es>
2026-09-15 13:39:18 +0200

    Initial Commit

---
+++ la1.cpp
@@ -0,0 +1,187 @@
+//============================================================================
+// Computational models
+// Name        : la1.cpp
+// Author      : Pedro A. Gutiérrez
+// Version     :
+// Copyright   : Universidad de Córdoba
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <iostream>
+#include <ctime>   // To obtain current time time()
+#include <cstdlib> // To establish the seed srand() and generate pseudorandom numbers rand()
+#include <string.h>
+#include <math.h>
+
+#include "mc/MultilayerPerceptron.h"
+#include "mc/util.h"
+#include <cfloat>
+
+using namespace mc;
+using namespace std;
+using namespace util;
+
+int main(int argc, char **argv)
+{
+    // Process arguments of the command line
+    bool Tflag = 0, wflag = 0, pflag = 0, tflag = 0, iflag = 0, lflag = 0, hflag = 0, eflag = 0, mflag = 0, nflag = 0;
+    char *Tvalue = NULL, *wvalue = NULL, *tvalue = NULL;
+    int ivalue = 1000, lvalue = 1, hvalue = 5;
+    float evalue = 0.1, mvalue = 0.9;
+    int c;
+
+    opterr = 0;
+
+    // a: Option that requires an argument
+    // a:: The argument required is optional
+    while ((c = getopt(argc, argv, "t:T:i:l:h:e:m:n::w:p")) != -1)
+    {
+        // The parameters needed for using the optional prediction mode of Kaggle have been included.
+        // You should add the rest of parameters needed for the lab assignment.
+        switch (c)
+        {
+        case 't':
+            tflag = true;
+            tvalue = optarg;
+        case 'T':
+            Tflag = true;
+            Tvalue = optarg;
+            break;
+        case 'i':
+            iflag = true;
+            ivalue = atoi(optarg);
+        case 'l':
+            lflag = true;
+            lvalue = atoi(optarg);
+        case 'h':
+            hflag = true;
+            hvalue = atoi(optarg);
+        case 'e':
+            eflag = true;
+            evalue = atof(optarg);
+        case 'm':
+            mflag = true;
+            mvalue = atof(optarg);
+        case 'n':
+            nflag = true;
+        case 'w':
+            wflag = true;
+            wvalue = optarg;
+            break;
+        case 'p':
+            pflag = true;
+            break;
+        case '?':
+            if (optopt == 't' or optopt == 'T' or optopt == 'i' or optopt == 'l' or optopt == 'h' or optopt == 'e' or optopt == 'm' || optopt == 'w' || optopt == 'p')
+                fprintf(stderr, "The option -%c requires an argument.\n", optopt);
+            else if (isprint(optopt))
+                fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+            else
+                fprintf(stderr,
+                        "Unknown character `\\x%x'.\n",
+                        optopt);
+            return EXIT_FAILURE;
+        default:
+            return EXIT_FAILURE;
+        }
+    }
+
+    if (!pflag)
+    {
+        //////////////////////////////////
+        // TRAINING AND EVALUATION MODE //
+        //////////////////////////////////
+
+        // Multilayer perceptron object
+        MultilayerPerceptron mlp;
+
+        // Parameters of the mlp. For example, mlp.eta = value;
+        mlp.eta = evalue;
+        mlp.mu = mvalue;
+
+        int iterations = ivalue;
+
+        // Read training and test data: call to util::readData(...)
+        Dataset *trainDataset = NULL; // This should be corrected
+        Dataset *testDataset = NULL;  // This should be corrected
+
+        // Scale dataset. Use the functions you have in util.h and util.cpp
+        // if (nflag) {
+        // ...
+        //}
+
+        // Initialize topology vector
+        int layers = lvalue;
+        int *topology = NULL; // This should be corrected
+
+        // Initialize the network using the topology vector
+        mlp.initialize(layers + 2, topology);
+
+        // Seed for random numbers
+        int seeds[] = {1, 2, 3, 4, 5};
+        double *testErrors = new double[5];
+        double *trainErrors = new double[5];
+        double bestTestError = DBL_MAX;
+        for (int i = 0; i < 5; i++)
+        {
+            cout << "**********" << endl;
+            cout << "SEED " << seeds[i] << endl;
+            cout << "**********" << endl;
+            srand(seeds[i]);
+            mlp.runOnlineBackPropagation(trainDataset, testDataset, iterations, &(trainErrors[i]), &(testErrors[i]));
+            cout << "We end!! => Final test error: " << testErrors[i] << endl;
+
+            // We save the weights every time we find a better model
+            if (wflag && testErrors[i] <= bestTestError)
+            {
+                mlp.saveWeights(wvalue);
+                bestTestError = testErrors[i];
+            }
+        }
+
+        cout << "WE HAVE FINISHED WITH ALL THE SEEDS" << endl;
+
+        double averageTestError = 0, stdTestError = 0;
+        double averageTrainError = 0, stdTrainError = 0;
+
+        // Obtain training and test averages and standard deviations
+
+        cout << "FINAL REPORT" << endl;
+        cout << "************" << endl;
+        cout << "Train error (Mean +- SD): " << averageTrainError << " +- " << stdTrainError << endl;
+        cout << "Test error (Mean +- SD):          " << averageTestError << " +- " << stdTestError << endl;
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+
+        //////////////////////////////
+        // PREDICTION MODE (KAGGLE) //
+        //////////////////////////////
+
+        // Multilayer perceptron object
+        MultilayerPerceptron mlp;
+
+        // Initializing the network with the topology vector
+        if (!wflag || !mlp.readWeights(wvalue))
+        {
+            cerr << "Error while reading weights, we can not continue" << endl;
+            exit(-1);
+        }
+
+        // Reading training and test data: call to util::readData(...)
+        Dataset *testDataset;
+        testDataset = readData(Tvalue);
+        if (testDataset == NULL)
+        {
+            cerr << "The test file is not valid, we can not continue" << endl;
+            exit(-1);
+        }
+
+        mlp.predict(testDataset);
+
+        return EXIT_SUCCESS;
+    }
+}
---
+++ mc/util.cpp
@@ -0,0 +1,155 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>  // To establish the seed srand() and generate pseudorandom numbers rand()
+
+#include "MultilayerPerceptron.h"
+#include "util.h"
+
+using namespace mc;
+using namespace std;
+using namespace util;
+
+
+// ------------------------------
+// Obtain an integer random number in the range [Low,High]
+int util::randomInt(int Low, int High)
+{
+	return rand() % (High-Low+1) + Low;
+}
+
+// ------------------------------
+// Obtain a real random number in the range [Low,High]
+double util::randomDouble(double Low, double High)
+{
+	return ((double) rand() / RAND_MAX) * (High-Low) + Low;
+}
+
+// ------------------------------
+// Read a dataset from a file name and return it
+Dataset *util::readData(const char *fileName)
+{
+
+    ifstream myFile(fileName); // Create an input stream
+
+    if (!myFile.is_open())
+    {
+        cout << "ERROR: I cannot open the file " << fileName << endl;
+        return NULL;
+    }
+
+    Dataset *dataset = new Dataset;
+    if (dataset == NULL)
+        return NULL;
+
+    string line;
+    int i, j;
+
+    if (myFile.good())
+    {
+        getline(myFile, line); // Read a line
+        istringstream iss(line);
+        iss >> dataset->nOfInputs;
+        iss >> dataset->nOfOutputs;
+        iss >> dataset->nOfPatterns;
+    }
+    dataset->inputs = new double *[dataset->nOfPatterns];
+    dataset->outputs = new double *[dataset->nOfPatterns];
+
+    for (i = 0; i < dataset->nOfPatterns; i++)
+    {
+        dataset->inputs[i] = new double[dataset->nOfInputs];
+        dataset->outputs[i] = new double[dataset->nOfOutputs];
+    }
+
+    i = 0;
+    while (myFile.good())
+    {
+        getline(myFile, line); // Read a line
+        if (!line.empty())
+        {
+            istringstream iss(line);
+            for (j = 0; j < dataset->nOfInputs; j++)
+            {
+                double value;
+                iss >> value;
+                if (!iss)
+                    return NULL;
+                dataset->inputs[i][j] = value;
+            }
+            for (j = 0; j < dataset->nOfOutputs; j++)
+            {
+                double value;
+                iss >> value;
+                if (!iss)
+                    return NULL;
+                dataset->outputs[i][j] = value;
+            }
+            i++;
+        }
+    }
+
+    myFile.close();
+
+    return dataset;
+}
+
+// ------------------------------
+// Transform an scalar x by scaling it to a given range [minAllowed, maxAllowed] considering the min
+// and max values of the feature in the dataset (minData and maxData). 
+double util::minMaxScaler(double x, double minAllowed, double maxAllowed, double minData, double maxData)
+{
+    // TODO
+}
+
+// ------------------------------
+// Scale the dataset inputs to a given range [minAllowed, maxAllowed] considering the min
+// and max values of the feature in the dataset (minData and maxData). 
+void util::minMaxScalerDataSetInputs(Dataset *dataset, double minAllowed, double maxAllowed,
+                                     double *minData, double *maxData)
+{
+    // TODO
+}
+
+// ------------------------------
+// Scale the dataset outputs to a given range [minAllowed, maxAllowed] considering the min
+// and max values of the output in the dataset (minData and maxData). 
+void util::minMaxScalerDataSetOutputs(Dataset *dataset, double minAllowed, double maxAllowed,
+                                      double *minData, double *maxData)
+{
+    // TODO
+}
+
+// ------------------------------
+// Extract maximum and minimum values from a matrix of doubles
+void util::obtainMaxMinValuesFromMatrix(double **matrix, int nRows, int nColumns, double *obtainedMins, double *obtainedMaxs)
+{
+
+    // TODO
+}
+
+// ------------------------------
+// Print the dataset
+void util::printDataset(Dataset *dataset, int len)
+{
+    if (len == 0)
+        len = dataset->nOfPatterns;
+    
+
+    for (int i = 0; i < len; i++)
+    {
+        cout << "P" << i << ":" << endl;
+        for (int j = 0; j < dataset->nOfInputs; j++)
+        {
+            cout << dataset->inputs[i][j] << ",";
+        }
+
+        for (int j = 0; j < dataset->nOfOutputs; j++)
+        {
+            cout << dataset->outputs[i][j] << ",";
+        }
+        cout << endl;
+    }
+    
+}
+
